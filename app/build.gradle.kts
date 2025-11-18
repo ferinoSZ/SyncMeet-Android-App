caa661616644d78da9cc8be4d8b61df093f31415
ferino.SZ <weslleycontas09@gmail.com>
2025-11-18 18:05:28 -0300

    Primeiro Commit do App
    -- Adicionado background com gradiente
    -- Adicionado a logo do SyncMeet
    -- Foi feito um container para o login
    -- Criado 2 input de login para o usuário com um underline
    -- Colocado ícones de acordo com o input
    -- um switch de lembrar de mim

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    // CORRETO: Chamando o apelido 'androidApplication' diretamente
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "com.example.syncmeet"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.syncmeet"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file...
+plugins {
+    // Correção: Usar o apelido "androidApplication" diretamente
+    alias(libs.plugins.androidApplication) apply false
+}
